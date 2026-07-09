6855a901e162f23d38b81af9089f2c9858e14989
Saad41119 <chatthasadi@gmail.com>
2026-07-09 14:20:55 +0200

    Initial commit: Flask demo app

---
+++ app.py
@@ -0,0 +1,23 @@
+﻿from flask import Flask, jsonify
+from datetime import datetime
+import socket
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return jsonify(
+        message="Hello from Flask on EC2!",
+        hostname=socket.gethostname(),
+        time=datetime.utcnow().isoformat() + "Z",
+    )
+
+
+@app.route("/health")
+def health():
+    return jsonify(status="ok"), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
